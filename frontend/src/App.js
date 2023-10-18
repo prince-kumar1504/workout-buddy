ff553e805eda70368e3772384be6f8e3f3996788
prince-kumar1504 <prince15raja04@gmail.com>
2023-10-18 16:18:13 +0530

    user-validation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 //pages & component
 import Home from './pages/Home'
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 function App() {
   return (
     <div className="App">
-     <BrowserRouter>
-     <Navbar/>
-     <div className='pages'>
-       <Routes>
-        <Route path="/" element={<Home/>}/>
-       </Routes>
-     </div>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <div className='pages'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
 
     </div>
   );
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,17 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <header>
-    <div className='container'>
+      <div className='container'>
         <Link to='/'>
-            <h1>Workout Buddy</h1>
+          <h1>Workout Buddy</h1>
         </Link>
-    </div>
+        <nav>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </nav>
+      </div>
     </header>
   )
 }
